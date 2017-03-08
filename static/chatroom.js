7638883e17efecca34c89fe7a8581507ccbc5bc3
Ben Jacobson <BenTJacobson@gmail.com>
2017-03-08 20:50:37 +0000

    add originator and new endpoint submission

--- static/chatroom.js
+++ static/chatroom.js
@@ -11,15 +11,15 @@ function guid() {
     s4() + '-' + s4() + s4() + s4();
 }
 
-originID = guid()
-nextMessageID = 0
-myEndpoint = 'https://' + window.location.hostname + '/gossip/' + originID
+var originID = guid()
+var nextMessageID = 0
+var myEndpoint = 'https://' + window.location.hostname + '/gossip/' + originID
+var myName = 'Anon';
 
-otherEndpoints = []
-
-messageStore = {}
-messageNeeds = {}
-lastMessageID = originID + ':' + nextMessageID
+var otherEndpoints = []
+var messageStore = {}
+var messageNeeds = {}
+var lastMessageID = originID + ':' + nextMessageID
 
 function getGossipEndpoints() {
 	var http = new XMLHttpRequest();
@@ -31,7 +31,13 @@ function getGossipEndpoints() {
 	    }
 	}
 	http.send();
-	setTimeout(getGossipEndpoints, 10000);
+}
+
+function addNewEndpoint() {
+	var newEndpointElement = document.getElementById('newEndpoint');
+	var newEndpoint = newEndpointElement.value;
+	otherEndpoints.push(newEndpoint);
+	newEndpointElement.value = '';
 }
 
 function scrollToRecentChatMessage() {
@@ -41,9 +47,10 @@ function scrollToRecentChatMessage() {
 	});
 }
 
-function addChatMessage(message) {
+function addChatMessage(name, message) {
+	if (name == undefined) name = 'Anon';
 	var li = document.createElement("li");
-	var messageNode = document.createTextNode(message);
+	var messageNode = document.createTextNode(name + ': ' + message);
 	li.appendChild(messageNode);
 	document.getElementById("messagesList").appendChild(li);
 	scrollToRecentChatMessage();
@@ -88,14 +95,10 @@ function propagateGossipMessage() {
 function receiveGossipMessage(gossipMessage) {
 	if (gossipMessage.hasOwnProperty('Rumor')) {
 		// update message needs here
-		try {
-			var originSeq = gossipMessage.Rumor.ID.split(':');
-		} catch (err) {
-			debugger;
-		}
+		var originSeq = gossipMessage.Rumor.ID.split(':');
 		if (!messageStore.hasOwnProperty(gossipMessage.Rumor.ID)) {
 			messageStore[gossipMessage.Rumor.ID] = gossipMessage.Rumor;
-			addChatMessage(gossipMessage.Rumor.Message);
+			addChatMessage(gossipMessage.Rumor.Originator, gossipMessage.Rumor.Message);
 		}
 		origin = originSeq[0];
 		seq = parseInt(originSeq[1]);
@@ -113,6 +116,7 @@ function receiveGossipMessage(gossipMessage) {
 				// prepare gossipMessage rumor and end it
 				rumor = {};
 				rumor.Rumor = messageStore[messageID];
+
 				rumor.Endpoint = myEndpoint
 				console.log('Sending response to want');
 				console.log(rumor);
@@ -143,18 +147,24 @@ function sendChatMessage() {
 
 	id = originID + ':' + nextMessageID++
 	lastMessageID = id
-	messageStore[id] = {ID: id, Message: message}
+	messageStore[id] = {ID: id, Originator: myName, Message: message}
 
-	addChatMessage(message)
+	addChatMessage(myName, message)
 }
 
 window.onload = function () {
+	document.getElementById('endpoint').innerHTML += originID;
 	document.getElementById("chatMessage")
 		.addEventListener("keyup", function(event) {
 		if (event.keyCode == 13) {
 			document.getElementById("sendMessage").click();
 		}
 	});
+	var chatNameElement = document.getElementById("chatName");
+	chatNameElement.addEventListener("keyup", function(event) {
+		myName = chatNameElement.value;
+	});
+	myName = chatNameElement.value;
 	getGossipEndpoints();
 	propagateGossipMessage();
 	retrieveGossipMessage();
